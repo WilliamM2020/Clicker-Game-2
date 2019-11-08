f42da7c401648649a0c643de7b69a8bfbbee397d
William Moolman <moolman.william@gmail.com>
2019-11-08 14:29:27 +0200

    Textures and Attraction.gd init edit

--- scripts/Attraction.gd
+++ scripts/Attraction.gd
@@ -6,9 +6,16 @@ var production
 var cost
 var upgradecost
 var fame
+var fame_requirements
 
 func _ready():
 	pass # Replace with function body.
 
 func _init(var id, var level):
-	pass
+	cost = $GlobalVariables.ATTRACTION_COST_SCALING^id
+	production = cost*$GlobalVariables.LEVEL_ONE_PRODUCTION*($GlobalVariables.UPGRADE_PRODUCTION_SCALING^(level-1))
+	upgradecost = production * $GlobalVariables.UPGRADE_COST_SCALING
+	fame_requirements = cost*$GlobalVariables.FAME_REQUIREMENT
+	fame = $GlobalVariables.FAME_INCREASE*cost*level
+	
+	

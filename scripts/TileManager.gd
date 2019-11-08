e3490bfaffaff6d109dd76bd6b7c6f649731e9df
James <jamoxploder@gmail.com>
2019-11-08 14:17:22 +0200

    Attraction / tilemanager work

---
+++ scripts/TileManager.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var tiles #array of vector2 coordinates which store where tiles are placed
+var attractions #array of vector2 coordinates which store where attractions are placed
+
+func _ready():
+	pass # Replace with function body.
+

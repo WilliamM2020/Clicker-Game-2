1a92a4bc9ec43f950acbcaf87b278eb184b7ded8
James <jamoxploder@gmail.com>
2019-11-09 12:44:35 +0200

    Can set tiles through code

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -3,12 +3,9 @@ extends Node2D
 var hold_pos
 
 func _ready():
-	var attraction = load("res://scenes/Attraction.tscn")
-	var new_attraction = attraction.instance()
-	new_attraction.set_name("Attraction")
-	add_child(new_attraction)
-	$Attraction.position = Vector2(256,190)
-	$Attraction.initialize(2,3)
+	$TileManager.set_tile(1,Vector2(0,0))
+	for tile in $TileManager.get_adjacent_tiles(Vector2(0,0)):
+		$TileManager.set_tile(2,tile)
 
 func _process(delta):
 	get_input()
@@ -19,3 +16,9 @@ func get_input():
 	if Input.is_action_pressed("mouse1"):
 		var offset = Vector2(hold_pos.x - get_global_mouse_position().x, hold_pos.y - get_global_mouse_position().y)
 		$Camera2D.translate(offset)
+	if Input.is_action_pressed("zoom_in"):
+		$Camera2D.set_size($Camera2D.size*1.1)
+		print("zoom in")
+	if Input.is_action_pressed("zoom_out"):
+		$Camera2D.set_size($Camera2D.size*0.9)
+		print("zoom out")
--- scripts/TileManager.gd
+++ scripts/TileManager.gd
@@ -1,8 +1,17 @@
 extends Node
 
+const tile_offset = Vector2(0,0)
+
 var tiles #array of vector2 coordinates which store where tiles are placed
-var attractions #array of vector2 coordinates which store where attractions are placed
+var attraction_pos #array of vector2 coordinates which store where attractions are placed
+var attractions #corresponds to attractions array and stores all the attractions
 
-func _ready():
-	pass
-
+func set_tile(var id, var position):
+	$TileMap.set_cellv(position, id)
+	
+func get_adjacent_tiles(var position):
+	var tiles = [position+Vector2(0,1),position+Vector2(0,-1),position+Vector2(1,0),position+Vector2(-1,0),position+Vector2(-1,1),position+Vector2(-1,-1)]
+	return tiles
+	
+func set_attraction(var attraction, var position):
+	attraction.position = $TileMap.map_to_world(position) + tile_offset

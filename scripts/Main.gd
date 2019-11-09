91234c38601232f7e043507c20d1e28839cf4681
James <jamoxploder@gmail.com>
2019-11-09 13:50:56 +0200

    Tile Selection working basic

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -6,6 +6,7 @@ func _ready():
 	$TileManager.set_tile(1,Vector2(0,0))
 	for tile in $TileManager.get_adjacent_tiles(Vector2(0,0)):
 		$TileManager.set_tile(2,tile)
+	$TileManager.select_tile(Vector2(0,1))
 
 func _process(delta):
 	get_input()
@@ -13,6 +14,7 @@ func _process(delta):
 func get_input():
 	if Input.is_action_just_pressed("mouse1"):
 		hold_pos = get_global_mouse_position()
+		$TileManager.attempt_selection()
 	if Input.is_action_pressed("mouse1"):
 		var offset = Vector2(hold_pos.x - get_global_mouse_position().x, hold_pos.y - get_global_mouse_position().y)
 		$Camera2D.translate(offset)
--- scripts/TileManager.gd
+++ scripts/TileManager.gd
@@ -1,13 +1,19 @@
 extends Node
 
-const tile_offset = Vector2(0,0)
+const tile_offset = Vector2(32,34)
+var selected_tile
 
 var tiles #array of vector2 coordinates which store where tiles are placed
 var attraction_pos #array of vector2 coordinates which store where attractions are placed
 var attractions #corresponds to attractions array and stores all the attractions
 
+var surround_tiles #array of tiles that surround currently placed tiles
+
 func set_tile(var id, var position):
 	$TileMap.set_cellv(position, id)
+	
+func get_tile_id(var position):
+	return $TileMap.get_cellv(position)
 	
 func get_adjacent_tiles(var position):
 	var tiles = [position+Vector2(0,1),position+Vector2(0,-1),position+Vector2(1,0),position+Vector2(-1,0),position+Vector2(-1,1),position+Vector2(-1,-1)]
@@ -15,3 +21,16 @@ func get_adjacent_tiles(var position):
 	
 func set_attraction(var attraction, var position):
 	attraction.position = $TileMap.map_to_world(position) + tile_offset
+	
+func attempt_selection():
+	var tile = $TileMap.world_to_map($TileMap.get_local_mouse_position())
+	select_tile(tile)
+	
+func select_tile(var position):
+	if get_tile_id(position) != TileMap.INVALID_CELL: #or position in surround_tiles:
+		selected_tile = position
+		$TileMap/SelectionHex.position = $TileMap.map_to_world(position) + tile_offset
+		$TileMap/SelectionHex.show()
+	else:
+		selected_tile = null
+		$TileMap/SelectionHex.hide()

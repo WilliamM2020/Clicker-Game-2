f37631f5c99685c157762336f56dcbd82fc35778
William Moolman <moolman.william@gmail.com>
2019-11-09 13:09:32 +0200

    Added Palace

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -6,6 +6,7 @@ func _ready():
 	$TileManager.set_tile(1,Vector2(0,0))
 	for tile in $TileManager.get_adjacent_tiles(Vector2(0,0)):
 		$TileManager.set_tile(2,tile)
+	$TileManager.set_tile(5,Vector2(3,-3))
 
 func _process(delta):
 	get_input()
